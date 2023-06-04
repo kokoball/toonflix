70dfee22565487d8c230d8c0a7361f2e7ea1c58d
kokoball <kbp4154@naver.com>
2023-06-04 23:06:24 +0900

    6.3 webtoon from.Json()

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
+import 'package:toonflix/services/api_service.dart';
 
 void main() {
+  ApiService.getTodaysToons();
   runApp(const App());
 }
 
@@ -10,17 +12,8 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(
-          textTheme: const TextTheme(
-              displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          )),
-          cardColor: const Color(0xFFF4EDDB),
-          colorScheme: const ColorScheme.light(
-            background: Color(0xFFE7626C),
-          ),
-        ),
-        home: const HomeScreen());
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
   }
 }
---
+++ lib/services/api_service.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
+
+class ApiService {
+  final String baseUrl = "http://webtoon-crawler.nomadcoders.workers.dev/";
+  final String today = "today";
+
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtooninstances = [];
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtooninstances.add(WebtoonModel.fromJson((webtoon)));
+      }
+      return webtooninstances;
+    }
+    throw Error();
+  }
+}
