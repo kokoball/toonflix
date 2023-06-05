4de8ba5ff80df63a33627710e9303e0e7ab4c1c0
kokoball <kbp4154@naver.com>
2023-06-05 23:49:47 +0900

    6.6 Fix api bug, Add futureBuilder widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/services/api_service.dart';
+import '../models/webtoon_model.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +22,15 @@ class HomeScreen extends StatelessWidget {
             fontWeight: FontWeight.w400,
           ),
         ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Text("There is Data!");
+          }
+          return const Text('Loading...');
+        },
       ),
     );
   }
